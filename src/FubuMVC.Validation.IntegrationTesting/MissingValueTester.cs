63514533924674d076b73359411a1e2c28a80069
Matt Sollars <ventaur@gmail.com>
2014-02-09 11:59:32 -0500

    New test to demonstrate StackOverflowException for input that is missing (not just empty) a required field.

---
+++ src/FubuMVC.Validation.IntegrationTesting/MissingValueTester.cs
@@ -0,0 +1,77 @@
+﻿using System.Diagnostics;
+
+using FubuMVC.Core;
+using FubuMVC.Validation.IntegrationTesting.Ajax;
+
+using FubuTestingSupport;
+
+using FubuValidation;
+
+using NUnit.Framework;
+
+
+namespace FubuMVC.Validation.IntegrationTesting
+{
+    [TestFixture]
+    public class MissingValueTester : ValidationHarness
+    {
+        protected override void configure(FubuRegistry registry)
+        {
+            registry.Actions.IncludeType<MissingValueEndpoint>();
+            registry.Import<FubuMvcValidation>();
+        }
+
+        private JsonResponse theContinuation
+        {
+            get
+            {
+                var badInput = new UnexpectedInputWithoutName
+                {
+                    Email = "someone@nowhere.com"
+                };
+                var response = endpoints.PostJson(new ExpectedInputWithName(), badInput);
+
+                try
+                {
+                    return response.ReadAsJson<JsonResponse>();
+                }
+                catch
+                {
+                    Debug.WriteLine(response.ReadAsText());
+                    throw;
+                }
+            }
+        }
+
+        [Test]
+        public void validation_fails_required_without_stackoverflow()
+        {
+            var response = theContinuation;
+            response.success.ShouldBeFalse();
+            response.errors.ShouldNotBeNull();
+            response.errors[0].message.ShouldEqual(ValidationKeys.Required.DefaultValue);
+        }
+    }
+
+
+    public class MissingValueEndpoint
+    {
+        public ExpectedInputWithName post_missing_value(ExpectedInputWithName input)
+        {
+            return input;
+        }
+    }
+
+
+    public class ExpectedInputWithName
+    {
+        [Required]
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+
+    public class UnexpectedInputWithoutName
+    {
+        public string Email { get; set; }
+    }
+}
